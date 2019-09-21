e36b0b2f2bceda023f0edda49b3ab53855a5ed68
Leonardo <leonardo.tavarez@necomplus.com>
2019-09-20 21:04:54 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+$numbers = [1,2,3,4,5,6,7,8,9];
+
+$blockNumbers = [4,5];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+     <link rel="stylesheet" type="text/css" href="assets\css\style.css">
+
+    <title>Multiplication Table</title>
+
+</head>
+<body>
+    
+
+
+</body>
+</html>
